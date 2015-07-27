e160561bb55b7c2f988920e55a471bd715d189f9
Patrick Fette <pdfett01@gmail.com>
2015-07-27 11:40:04 -0400

    added picture to registration page, made style a drop down option

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,2 +1,4 @@
 class Contestant < ActiveRecord::Base
+
+	STYLE = ['Fake Creative', 'Fake Realistic','Natural Mustache', 'Styled Mustache', 'Whaler', 'Goatee', 'Chops', 'Partial Beard Freestyle', 'Full Beard 0-5 Inches', 'Full Beard 5-10 Inches', 'Full Beard 10+ Inches', 'Full Beard Freestyle', 'Full Beard Styled Mustache']
 end
